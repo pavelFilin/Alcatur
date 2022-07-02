04d8321d393b9230cfc1cbcedf43aea29a69005a
Pavel Filin <pavel164rus1@gmail.com>
2022-07-02 15:29:16 +0400

    init commit

---
+++ src/main/groovy/ru/filin/alcatur/AlcaturApplication.groovy
@@ -0,0 +1,13 @@
+package ru.filin.alcatur
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AlcaturApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(AlcaturApplication, args)
+    }
+
+}
